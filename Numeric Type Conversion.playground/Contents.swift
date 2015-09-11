8223db04b3c509b20c579d5b9a5e73aa86dc0526
Stanley Diaz <stanley.diaz@papaly.com>
2015-09-10 20:13:38 -0700

    added numeric type conversion

---
+++ Numeric Type Conversion.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+// Numberic type conversion fancy way of saying we can convert Int to Dbls and dbls to Int
+var intValue = 5
+var doubleValue = 0.5
+
+
+var sumOfValues = Double(intValue) + doubleValue
+
+var secondDoubleValue = 1.5
+var secondIntegerValue = 8
+
+var secondSumOfValues = Int(secondDoubleValue) + secondIntegerValue
+// Numeric type conversion: you will use Double() to pass in an Int
+// or you will use Int() to pass in a double
